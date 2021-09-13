ab0b41c832e1986df76a8005804de87222046c25
cui-dewen <88224714+cui-dewen@users.noreply.github.com>
2021-09-13 16:13:55 +0900

    Update s.py

--- s.py
+++ s.py
@@ -1,6 +1,1 @@
-from slack_sdk import WebClient
-
-client = WebClient(token='xoxb-247578397445-2237502186229-eld4CjB9UBfKCM0t2c5LHNOY')
-#client.chat_postMessage(channel="myserver",text="djkhfjka")
-r=client.files_upload(channel="myserver",file="./t.py")
-print(r)
+print("上手")
