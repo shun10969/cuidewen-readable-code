d5bf4af3285718ce3031b085b84aed2492e5dfd3
Shunsuke Arai <shuna10969@gmail.com>
2021-09-14 14:13:31 +0900

    仕様5の実装

--- command.py
+++ command.py
@@ -3,5 +3,4 @@ import dictionary_data
 # print(dictionary_data.language1)
 
 for i in range(len(dictionary_data.language_list)):
-    print(dictionary_data.language_list[i])
-
+    print(i+1,": ",dictionary_data.language_list[i])
