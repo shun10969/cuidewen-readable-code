efbc967ac55dabf5548dace9ed697f1ed39cec02
Shunsuke Arai <shuna10969@gmail.com>
2021-09-14 14:07:30 +0900

    仕様4の実装

--- command.py
+++ command.py
@@ -1,3 +1,7 @@
 import dictionary_data
 
-print(dictionary_data.language1)
+# print(dictionary_data.language1)
+
+for i in range(len(dictionary_data.language_list)):
+    print(dictionary_data.language_list[i])
+
--- dictionary_data.py
+++ dictionary_data.py
@@ -1,1 +1,5 @@
 language1 = "上手"
+language2 = "市場"
+language3 = "一時"
+
+language_list = [language1,language2,language3]
