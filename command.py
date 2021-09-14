8d99151b984063c80af5aad46bf0f3ac4bed7faf
Shunsuke Arai <shuna10969@gmail.com>
2021-09-14 14:34:31 +0900

    仕様6の実装

--- command.py
+++ command.py
@@ -2,5 +2,11 @@ import dictionary_data
 
 # print(dictionary_data.language1)
 
-for i in range(len(dictionary_data.language_list)):
-    print(i+1,": ",dictionary_data.language_list[i])
+input_id = input('>>')
+# print(input_id)
+
+print(int(input_id),": ",dictionary_data.language_list[int(input_id)])
+
+
+# for i in range(len(dictionary_data.language_list)):
+#     print(i+1,": ",dictionary_data.language_list[i])
