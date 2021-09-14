e2a5302b3f8fdeab4de0d5be23ac709444404623
Shunsuke Arai <shuna10969@gmail.com>
2021-09-14 14:42:05 +0900

    仕様6の実装 (細かい修正)

--- README.txt
+++ README.txt
@@ -3,4 +3,5 @@ python
 
 1. dictionary_data.pyをcommand.pyファイルにimport
 2. dictionary_dataの中のlanguage1-3をprint関数の引数に使用
-3. ファイルの実行
+3. 参照したい辞書データのidを入力してもらう
+4. 入力したidにしたがって、単語データを出力
--- command.py
+++ command.py
@@ -1,12 +1,8 @@
 import dictionary_data
 
-# print(dictionary_data.language1)
-
 input_id = input('>>')
-# print(input_id)
 
 print(int(input_id),": ",dictionary_data.language_list[int(input_id)])
-
 
 # for i in range(len(dictionary_data.language_list)):
 #     print(i+1,": ",dictionary_data.language_list[i])
